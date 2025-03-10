e08e7e21f2200aece69a8f01bae14a714cfc9e34
ClauLopez2 <IAQ29709@educastur.es>
2025-03-10 16:06:07 +0100

    NivelUnoEInterfaz

--- Assets/Scripts/Space.cs
+++ Assets/Scripts/Space.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Cambio de escena
+using UnityEngine.SceneManagement; // Necesario para cambiar de escena
 
-public class Space : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
-    public string sceneName; // Nombre de la escena a cargar
+    public string spaceSceneName; // Escena a cargar al presionar Space
+    public string escapeSceneName; // Escena a cargar al presionar Esc
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) // Si se presiona la barra espaciadora
+        if (Input.GetKeyDown(KeyCode.Space)) // Si se presiona SPACE
         {
-            ChangeLevel(); // Cambia de escena
+            ChangeLevel(spaceSceneName);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) // Si se presiona ESC
+        {
+            ChangeLevel(escapeSceneName);
         }
     }
 
     public void OnMouseDown()
     {
         Debug.Log("Hago click"); // Mensaje en la consola
-        ChangeLevel(); // Llama al cambio de escena
+        ChangeLevel(spaceSceneName); // Cambia de escena al hacer clic
     }
 
-    public void ChangeLevel() // Método para cambiar de escena
+    public void ChangeLevel(string sceneName) // Método para cambiar de escena
     {
         if (!string.IsNullOrEmpty(sceneName))
         {
@@ -33,4 +39,6 @@ public class Space : MonoBehaviour
         }
     }
 }
+
+
 
